d41b789bb6c88a532f2e7fc8f5e5f36f532335ff
panggil aku shifa <87982800+Veronikamay@users.noreply.github.com>
2021-07-30 07:55:17 +0700

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -11,7 +11,7 @@ from helper_func import encode
 
 @Bot.on_message(filters.private & filters.user(ADMINS) & ~filters.command(['start','batch','genlink']))
 async def channel_post(client: Client, message: Message):
-    reply_text = await message.reply_text("Tunggu...!", quote = True)
+    reply_text = await message.reply_text("𝐓𝐔𝐍𝐆𝐆𝐔 𝐒𝐄𝐁𝐄𝐍𝐓𝐀𝐑 ...", quote = True)
     try:
         post_message = await message.copy(chat_id = client.db_channel.id, disable_notification=True)
     except FloodWait as e:
@@ -19,16 +19,16 @@ async def channel_post(client: Client, message: Message):
         post_message = await message.copy(chat_id = client.db_channel.id, disable_notification=True)
     except Exception as e:
         print(e)
-        await reply_text.edit_text("Something went Wrong..!")
+        await reply_text.edit_text("𝐀𝐃𝐀 𝐘𝐀𝐍𝐆 𝐒𝐀𝐋𝐀𝐇, 𝐌𝐎𝐇𝐎𝐍 𝐔𝐋𝐀𝐍𝐆𝐈 ...!")
         return
     converted_id = post_message.message_id * abs(client.db_channel.id)
     string = f"get-{converted_id}"
     base64_string = await encode(string)
     link = f"https://t.me/{client.username}?start={base64_string}"
 
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 𝔹𝔸𝔾𝕀𝕂𝔸ℕ", url=f'https://telegram.me/share/url?url={link}')]])
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("𝐁𝐀𝐆𝐈𝐊𝐀𝐍 𝐋𝐈𝐍𝐊", url=f'https://telegram.me/share/url?url={link}')]])
 
-    await reply_text.edit(f"<b>INI LINK MU</b>\n\n{link}", reply_markup=reply_markup, disable_web_page_preview = True)
+    await reply_text.edit(f"<b>INI LINK FILEMU</b>\n\n{link}", reply_markup=reply_markup, disable_web_page_preview = True)
 
     if not DISABLE_CHANNEL_BUTTON:
         await post_message.edit_reply_markup(reply_markup)
@@ -43,7 +43,7 @@ async def new_post(client: Client, message: Message):
     string = f"get-{converted_id}"
     base64_string = await encode(string)
     link = f"https://t.me/{client.username}?start={base64_string}"
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 𝔹𝔸𝔾𝕀𝕂𝔸ℕ", url=f'https://telegram.me/share/url?url={link}')]])
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("𝐁𝐀𝐆𝐈𝐊𝐀𝐍 𝐋𝐈𝐍𝐊", url=f'https://telegram.me/share/url?url={link}')]])
     try:
         await message.edit_reply_markup(reply_markup)
     except Exception as e:
